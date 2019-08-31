81c5bc4ddc508267178474a01c3053179af977e6
Shichiyou <shichiyou@outlook.com>
2019-08-31 22:58:37 +0900

    unstated-next install.

---
+++ src/Counter.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { createContainer } from "unstated-next";
+
+const useCounter = (initialState = 0) => {
+  let [count, setCount] = useState(initialState);
+  let decrement = () => setCount(count - 1);
+  let increment = () => setCount(count + 1);
+  return { count, decrement, increment };
+}
+
+const Counter = createContainer(useCounter);
+
+const CounterDisplay = () => {
+  let counter = Counter.useContainer();
+  return (
+    <div>
+      <button onClick={counter.decrement}>-</button>
+      <span>{counter.count}</span>
+      <button onClick={counter.increment}>+</button>
+    </div>
+  );
+}
+
+export const CounterUI = () => {
+    return (
+      <Counter.Provider>
+        <CounterDisplay />
+        <Counter.Provider initialState={2}>
+          <div>
+            <div>
+              <CounterDisplay />
+            </div>
+          </div>
+        </Counter.Provider>
+      </Counter.Provider>
+    );
+}
+
+export default CounterDisplay;
