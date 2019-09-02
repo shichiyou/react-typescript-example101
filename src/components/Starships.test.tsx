324ebb6dce2103df9b2e2c932999f901a4bc3214
Shichiyou <shichiyou@outlook.com>
2019-09-02 20:36:32 +0900

    use fetch-mock

--- src/components/Starships.test.tsx
+++ src/components/Starships.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { render, cleanup, fireEvent, waitForElement, waitForDomChange, getByText } from '@testing-library/react'
-
+import { render, cleanup, fireEvent, waitForElement, waitForDomChange, getByText, waitForElementToBeRemoved } from '@testing-library/react'
+import fetchMock from 'fetch-mock';
 import Starships from './Starships';
+import responseData from '../services/Starships.json';
 
 afterEach(cleanup);
+
+beforeAll(() => {
+  fetchMock.get('https://swapi.co/api/starships', JSON.stringify(responseData));
+});
+afterAll(() => {
+  fetchMock.restore();
+});
 
 test('<Starships />', async () => {
   const { container, getByText, debug } = render(<Starships />, )
@@ -13,11 +21,14 @@ test('<Starships />', async () => {
   // </div>
   // you can also pass an element: debug(getByTestId('messages'))
 
-  expect(getByText(/^Loading...$/i).innerHTML).toBe('Loading...');
-  const el = await waitForElement(() =>  getByText(/^Death Star$/i));
+  const el = getByText(/^Loading...$/i).parentElement;
+  await waitForElementToBeRemoved(() => getByText(/^Loading...$/i))
+  // expect(getByText(/^Executor$/i)).toBe('Loading...');
+  expect(el!.childElementCount).toBe(1);
+  // const el = await waitForElement(() =>  getByText(/^Death Star$/i));
   // await waitForDomChange(debug);
 
-  expect(el.innerHTML).toBe('Death Star');
+  // expect(el.innerHTML).toBe('Death Star');
   // debug()
 });
 
