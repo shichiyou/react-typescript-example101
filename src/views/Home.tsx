ce8131a138d361ecaa105c56f65263f6ae13bfed
田中 康信 <ya-tanaka@i-enter.co.jp>
2019-09-09 10:21:05 +0900

    table home.

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,20 +1,115 @@
 import React from 'react';
-import { Paper } from '@material-ui/core';
-import Table from '../components/Table/Table';
+import { withStyles, Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import useConfigurationService from 'services/useConfigurationService';
+import useStoresService from 'services/useStoresService';
+
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }),
+)(TableCell);
+
+const StyledTableRow = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.background.default,
+      },
+    },
+  }),
+)(TableRow);
+
+// function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+//   return { name, calories, fat, carbs, protein };
+// }
+
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      marginTop: theme.spacing(3),
+      overflowX: 'auto',
+    },
+    table: {
+      minWidth: 700,
+    },
+  }),
+);
 
 const Home: React.FC = () => {
+  const classes = useStyles();
+  const config = useConfigurationService();
+  const service = useStoresService();
+
   return (
-    <Paper style={{ margin: '50px', padding: '50px' }}>
-      <Table
-        tableHeaderColor="warning"
-        tableHead={["ID", "Name", "Salary", "Country"]}
-        tableData={[
-          ["1", "Dakota Rice", "$36,738", "Niger"],
-          ["2", "Minerva Hooper", "$23,789", "Curaçao"],
-          ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
-          ["4", "Philip Chaney", "$38,735", "Korea, South"]
-        ]}
-      />
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          {config.status === 'loading' && 
+            (
+              <TableRow>
+                <StyledTableCell>Loading...</StyledTableCell>
+              </TableRow>              
+            )
+          }
+          {config.status === 'loaded' &&
+            <TableRow>
+              <StyledTableCell>店舗コード</StyledTableCell>
+              <StyledTableCell>適用日</StyledTableCell>
+              {config.payload.propertiyDefines!.map(define => (
+                <StyledTableCell>{define.displayName}</StyledTableCell>
+              ))}
+            </TableRow>              
+          }
+        </TableHead>
+        <TableBody>
+          {service.status === 'loading' && 
+            (
+              <TableRow>
+                <StyledTableCell>Loading...</StyledTableCell>
+              </TableRow>              
+            )
+          }
+          {config.status === 'loaded' && service.status === 'loaded' &&
+            service.payload.items!.map(store => 
+              store.propertyStatus!.map(row => 
+                (<StyledTableRow>
+                  <StyledTableCell component="th" scope="row">
+                    {store.storeCode}
+                  </StyledTableCell>
+                  <StyledTableCell>{row.effectiveDate!.toLocaleDateString()}</StyledTableCell>
+                  {config.payload.propertiyDefines!.map(define => {
+                    const item = row.properties!.find(x => x.propertyId === define.id);
+                    return (
+                      <StyledTableCell>{item!.propertyValue}</StyledTableCell>
+                    );                    
+                  })}
+                </StyledTableRow>)
+              )
+            )
+          }
+        </TableBody>
+      </Table>
     </Paper>
   );
 }
